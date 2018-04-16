9119f042f104a38b893b1224aa0a9a58e7c9bac1
MaxDrljic <max_spree@hotmail.com>
2018-04-16 20:17:09 +0200

    Created register route, now able to register email and password with proper validation, data gets saved to sqllite. Also, simple front-end created for register route with Vuetify

---
+++ server/src/models/index.js
@@ -0,0 +1,29 @@
+const fs = require('fs')
+const path = require('path')
+const Sequelize = require('sequelize')
+const config = require('../config/config')
+const db = {}
+
+const sequelize = new Sequelize(
+  config.db.database,
+  config.db.user,
+  config.db.password, {
+    dialect: 'sqlite'
+  },
+  config.db.option
+)
+
+fs
+  .readdirSync(__dirname)
+  .filter((file) =>
+    file !== 'index.js'
+  )
+  .forEach((file) => {
+    const model = sequelize.import(path.join(__dirname, file))
+    db[model.name] = model
+  })
+
+db.sequelize = sequelize
+db.Sequelize = Sequelize
+
+module.exports = db
