018c784593a74efecb818f02ace3578dd3e800a7
MaxDrljic <max_spree@hotmail.com>
2018-04-17 20:24:16 +0200

    Created UI for Register form & Fixed Navigation

---
+++ client/src/components/Header.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-toolbar fixed class="cyan" dark>
+    <v-toolbar-title class="mr-4">
+      <span
+        class="home"
+        @click="navigateTo({name: 'root'})">
+        TabTracker
+      </span>
+    </v-toolbar-title>
+
+    <!--- TODO: Implement Me -->
+    <!-- <v-toolbar-items>
+      <v-btn flat dark>
+        Browse
+      </v-btn>
+    </v-toolbar-items> -->
+
+    <v-spacer></v-spacer>
+
+    <v-toolbar-items>
+      <v-btn flat dark
+        @click="navigateTo({name: 'register'})">
+          Sign up
+      </v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  methods: {
+    navigateTo (route) {
+      this.$router.push(route)
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .home {
+    cursor: pointer;
+  }
+
+  .home:hover {
+    color: #E9E;
+  }
+</style>
--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,27 +1,26 @@
 <template>
-  <v-layout column>
-    <v-flex xs6 offset-xs3>
+  <v-layout justify-center>
+    <v-flex xs6>
       <div class="white elevation-2">
         <v-toolbar flat dense class="cyan" dark>
           <v-toolbar-title>Register</v-toolbar-title>
         </v-toolbar>
 
         <div class="pl-4 pr-4 pt-2 pb-2">
-          <input
-            type="email"
-            name="email"
+          <v-text-field
+            label="Email"
             v-model="email"
-            placeholder="email" />
+          ></v-text-field>
           <br>
-          <input
-            type="password"
-            name="password"
+          <v-text-field
+            label="Password"
             v-model="password"
-            placeholder="password" />
+          ></v-text-field>
           <br>
           <div class="error" v-html="error" />
           <br>
           <v-btn
+            dark
             class="cyan"
             @click="register">
             Register
