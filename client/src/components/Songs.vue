491adb32fb224bba23ce3c9c9168419f522fb44f
MaxDrljic <max_spree@hotmail.com>
2018-04-19 11:40:20 +0200

    Created Panel subcomponent which is applied in login, register and songs component, did small UI refactoring.

---
+++ client/src/components/Songs.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-layout justify-center>
+    <v-flex xs6>
+      <panel title="Songs">
+
+      </panel>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import Panel from '@/components/Panel'
+export default {
+  components: {
+    Panel
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
